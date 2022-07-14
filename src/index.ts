f39e7e96ecdde4e0f5b56994262d97f5b240e3b9
Marco <marcoabricot@gmail.com>
2022-07-14 23:03:57 +0200

    Feat(core): Added debug option

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
 import { Playlists } from "./api/playlists";
 
+type Options = { debug: boolean }
+
 export class Client {
     public token: String;
     public playlists: Playlists;
+    public options: Options
 
-    constructor(token) {
-      this.token = token;
+    constructor(token, options: Options = { debug: false }) {
+        this.token = token;
+        this.options = options;
 
-      this.playlists = new Playlists(this)
+        this.playlists = new Playlists(this)
     }
 }
