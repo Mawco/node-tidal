1eec5b377657cc2def025a1a3b712156639f5813
glazk0 <43091603+glazk0@users.noreply.github.com>
2022-07-19 21:27:56 +0200

    style: lint

--- test/test.ts
+++ test/test.ts
@@ -1,14 +1,14 @@
 import { Client } from '../src/index';
 
-import * as mocha from 'mocha';
 import * as chai from 'chai';
 
 const expect = chai.expect;
+
 describe('TIDAL.js', () => {
-
-  it('test' , () => {
+  it('test', () => {
     // haven't done the tests yet :/
-    expect(new Client('test')).to.have.a.property('token').that.equals('test');
+    expect(new Client({ token: 'test' }))
+      .to.have.a.property('token')
+      .that.equals('test');
   });
-
 });
