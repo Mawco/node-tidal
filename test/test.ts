c95eb7e2f9a9ef367651dd202b416b24874d1b37
Marco <marcoabricot@gmail.com>
2022-07-19 21:57:56 +0200

    Update test.ts

--- test/test.ts
+++ test/test.ts
@@ -4,11 +4,13 @@ import * as chai from 'chai';
 
 const expect = chai.expect;
 
-describe('TIDAL.js', () => {
-  it('test', () => {
-    // haven't done the tests yet :/
-    expect(new Client({ token: 'test' }))
-      .to.have.a.property('token')
-      .that.equals('test');
-  });
-});
+//TODO: make proper tests
+
+// describe('TIDAL.js', () => {
+//   it('test', () => {
+//     // haven't done the tests yet :/
+//     expect(new Client({ token: 'test' }))
+//       .to.have.a.property('token')
+//       .that.equals('test');
+//   });
+// });
